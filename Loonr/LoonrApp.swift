aab6806b0fcc99977637da24eef408961b1bc9e1
Anne Cahalan <northofnormal@gmail.com>
2026-01-05 11:14:41 -0500

    Initial Commit

---
+++ Loonr/LoonrApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoonrApp.swift
+//  Loonr
+//
+//  Created by Anne Cahalan on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct LoonrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoonrTests/LoonrTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoonrTests.swift
+//  LoonrTests
+//
+//  Created by Anne Cahalan on 1/5/26.
+//
+
+import Testing
+@testable import Loonr
+
+struct LoonrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
