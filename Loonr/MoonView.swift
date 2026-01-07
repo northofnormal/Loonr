3ad90cc724981b41e8b877ec80a930af63405031
Anne Cahalan <northofnormal@gmail.com>
2026-01-07 15:07:27 -0500

    moon phase ui

--- Loonr/MoonView.swift
+++ Loonr/MoonView.swift
@@ -16,15 +16,50 @@ class MoonViewModel: ObservableObject {
             moonData = try await MoonAPI().fetchMoonData()
         }
     }
+
+    func convertToReadable(date: String) -> String? {
+        let isoFormatter = ISO8601DateFormatter()
+        if let date = isoFormatter.date(from: date) {
+            let readableDateString = date.formatted(date: .abbreviated, time: .shortened)
+            return (readableDateString)
+        }
+
+        return nil
+    }
+
+    func covertToMoonSymbol() -> String {
+        guard let phase = moonData?.moonphaseText else { return "moonphase.newmoon" }
+        let downString = phase.lowercased()
+        let noSpacesString = downString.replacingOccurrences(of: " ", with: ".")
+        let sfSymbolsString = "moonphase." + noSpacesString
+
+        return sfSymbolsString
+    }
+
 }
 
 struct MoonView: View {
     @StateObject var viewModel = MoonViewModel()
 
     var body: some View {
-        Text("Hello Moon!")
-            .onAppear {
-                viewModel.fetchMoonData()
+        VStack(spacing: 10) {
+
+            Image(systemName: viewModel.covertToMoonSymbol())
+                .font(.system(size: 100))
+
+            Text(viewModel.moonData?.moonphaseText ?? "Loading...")
+                .font(Font.largeTitle.bold())
+
+            if let moonData = viewModel.moonData {
+                Text("Moon Rise: \(viewModel.convertToReadable(date: moonData.moonrise) ?? "")")
+                Text("Moon Set: \(viewModel.convertToReadable(date: moonData.moonset) ?? "")")
             }
+
+            Spacer()
+        }
+        .padding(10)
+        .onAppear {
+            viewModel.fetchMoonData()
+        }
     }
 }
